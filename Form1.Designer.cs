a59b8c05da23b741c2f23f8d9a60a8a4b849ffd8
diegobias <biasatti.diego@gmail.com>
2022-04-26 14:04:55 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace fiscales2da
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cb = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_iniciar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboIp = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboPeriodo = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.anio_numerico = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl_tiempo = new System.Windows.Forms.Label();
+            this.timer_tiempo = new System.Windows.Forms.Timer(this.components);
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.anio_numerico)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cb
+            // 
+            this.cb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb.FormattingEnabled = true;
+            this.cb.Location = new System.Drawing.Point(12, 41);
+            this.cb.Name = "cb";
+            this.cb.Size = new System.Drawing.Size(280, 28);
+            this.cb.TabIndex = 1;
+            this.cb.SelectedIndexChanged += new System.EventHandler(this.cb_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MES";
+            // 
+            // btn_iniciar
+            // 
+            this.btn_iniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_iniciar.Location = new System.Drawing.Point(11, 240);
+            this.btn_iniciar.Name = "btn_iniciar";
+            this.btn_iniciar.Size = new System.Drawing.Size(282, 69);
+            this.btn_iniciar.TabIndex = 3;
+            this.btn_iniciar.Text = "I N I C I A R";
+            this.btn_iniciar.UseVisualStyleBackColor = true;
+            this.btn_iniciar.Click += new System.EventHandler(this.btn_iniciar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(191, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "AÑO";
+            // 
+            // comboIp
+            // 
+            this.comboIp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboIp.FormattingEnabled = true;
+            this.comboIp.Location = new System.Drawing.Point(11, 172);
+            this.comboIp.Name = "comboIp";
+            this.comboIp.Size = new System.Drawing.Size(280, 28);
+            this.comboIp.TabIndex = 6;
+            this.comboIp.SelectedIndexChanged += new System.EventHandler(this.comboIp_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "IP ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "PERIODO";
+            // 
+            // comboPeriodo
+            // 
+            this.comboPeriodo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboPeriodo.FormattingEnabled = true;
+            this.comboPeriodo.Location = new System.Drawing.Point(12, 106);
+            this.comboPeriodo.Name = "comboPeriodo";
+            this.comboPeriodo.Size = new System.Drawing.Size(280, 28);
+            this.comboPeriodo.TabIndex = 8;
+            this.comboPeriodo.SelectedIndexChanged += new System.EventHandler(this.comboPeriodo_SelectedIndexChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 206);
+            this.progressBar1.Minimum = 1;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(281, 28);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 10;
+            this.progressBar1.Value = 1;
+            this.progressBar1.Visible = false;
+            // 
+            // anio_numerico
+            // 
+            this.anio_numerico.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.anio_numerico.Location = new System.Drawing.Point(233, 9);
+            this.anio_numerico.Maximum = new decimal(new int[] {
+            2100,
+            0,
+            0,
+            0});
+            this.anio_numerico.Minimum = new decimal(new int[] {
+            2015,
+            0,
+            0,
+            0});
+            this.anio_numerico.Name = "anio_numerico";
+            this.anio_numerico.Size = new System.Drawing.Size(61, 26);
+            this.anio_numerico.TabIndex = 11;
+            this.anio_numerico.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.anio_numerico.Value = new decimal(new int[] {
+            2015,
+            0,
+            0,
+            0});
+            this.anio_numerico.ValueChanged += new System.EventHandler(this.anio_numerico_ValueChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(308, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(449, 268);
+            this.textBox1.TabIndex = 12;
+            // 
+            // lbl_tiempo
+            // 
+            this.lbl_tiempo.AutoSize = true;
+            this.lbl_tiempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_tiempo.Location = new System.Drawing.Point(542, 11);
+            this.lbl_tiempo.Name = "lbl_tiempo";
+            this.lbl_tiempo.Size = new System.Drawing.Size(69, 20);
+            this.lbl_tiempo.TabIndex = 14;
+            this.lbl_tiempo.Text = "TIEMPO";
+            // 
+            // timer_tiempo
+            // 
+            this.timer_tiempo.Interval = 1000;
+            this.timer_tiempo.Tick += new System.EventHandler(this.timer_tiempo_Tick);
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_status.Location = new System.Drawing.Point(63, 206);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(123, 29);
+            this.lbl_status.TabIndex = 15;
+            this.lbl_status.Text = "PERIODO";
+            this.lbl_status.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(344, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(192, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "TIEMPO DE EJECUCION";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 315);
+            this.Controls.Add(this.lbl_status);
+            this.Controls.Add(this.lbl_tiempo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.anio_numerico);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboPeriodo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboIp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_iniciar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cb);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fiscales 2G -  2020";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.anio_numerico)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox cb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_iniciar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboIp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboPeriodo;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.NumericUpDown anio_numerico;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl_tiempo;
+        private System.Windows.Forms.Timer timer_tiempo;
+        private System.Windows.Forms.Label lbl_status;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace fiscales2da
+{
+    public partial class Form1 : Form
+    {
+        int nroMes, anio;
+        int tiempo = 0;
+        string _punto_venta = "", _fecha_archivo = "", _ip = "", _fecha = "";
+        string s1_inicio, s1_fin, s2_inicio, s2_fin, s3_inicio, s3_fin, s4_inicio, s4_fin;
+
+        string[] meses = {"SELECCIONAR","ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO",
+                              "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"
+                             };
+        StreamReader lector;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime fechaActual = DateTime.Today;
+            anio_numerico.Value = fechaActual.Year;
+            anio = Convert.ToInt32(anio_numerico.Value);
+
+            s1_inicio = "01"; s1_fin = "07";
+            s2_inicio = "08"; s2_fin = "14";
+            s3_inicio = "15"; s3_fin = "21";
+            s4_inicio = "22";
+
+            cb.Items.Clear();
+            cb.SelectedItem = meses[0];
+
+            for (int i = 0; i < meses.Length; i++)
+            {
+                cb.Items.Add(meses[i]);
+            }
+
+            //cargo el combo de las IP y asigno solamente la IP con la posicion 1 despues de pasar por el Split
+            llenoComboTxt("ip.txt", ':', comboIp, 1);
+
+        }
+
+        private void comboPeriodo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //En base al periodo seleccionado, armo parte del nombre del archivo .
+
+            string anioMes = anio.ToString() + "-" + nroMes.ToString("D2") + "-";
+            string[] lines = comboPeriodo.Text.Split(new string[] { "al" }, StringSplitOptions.None);
+            _fecha = anioMes + lines[0].Trim() + "-al-"+anioMes +lines[1].Trim();
+            _fecha_archivo = anio.ToString().Remove(0, 2) + nroMes.ToString("D2") + lines[0].Trim() + " " + anio.ToString().Remove(0, 2) + nroMes.ToString("D2") + lines[1].Trim();
+        }
+
+        private void timer_tiempo_Tick(object sender, EventArgs e)
+        {
+            tiempo++;
+            
+            TimeSpan time = TimeSpan.FromSeconds(tiempo);
+            lbl_tiempo.Text = time.ToString(@"mm\:ss");
+        }
+
+        private void anio_numerico_ValueChanged(object sender, EventArgs e)
+        {
+            anio = Convert.ToInt32(anio_numerico.Value);
+        }
+
+        private void cb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            nroMes = cb.SelectedIndex;
+            
+            if (nroMes != 0)
+            {
+
+                int diaMes = System.DateTime.DaysInMonth(anio, nroMes);
+                switch (diaMes)
+                {
+                    case 28:
+                        s4_fin = "28";
+                        break;
+                    case 29:
+                        s4_fin = "29";
+                        break;
+                    case 30:
+                        s4_fin = "30";
+                        break;
+                    case 31:
+                        s4_fin = "31";
+                        break;
+                }
+               
+                comboPeriodo.Items.Clear();
+                comboPeriodo.Items.Add(s1_inicio + " al " + s1_fin);
+                comboPeriodo.Items.Add(s2_inicio + " al " + s2_fin);
+                comboPeriodo.Items.Add(s3_inicio + " al " + s3_fin);
+                comboPeriodo.Items.Add(s4_inicio + " al " + s4_fin);
+            }
+            else
+            {
+                MessageBox.Show("Debe Seleccionar un Mes Válido.");
+                
+            }
+        }
+        private void comboIp_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] lines = comboIp.Text.Split(new string[] { " --> " }, StringSplitOptions.None);
+            _punto_venta = lines[1];
+            _ip = lines[0];
+        }
+       
+        private void llenoComboTxt(string archivoTxt, char CaracterSplit, ComboBox nombreCombo, int posicion )
+        {
+            lector = new StreamReader(archivoTxt);
+            
+            while (lector.Peek() >= 0)
+            {
+                var datos = lector.ReadLine().Split(CaracterSplit);
+                string ip = datos[posicion];
+                string nombre = datos[0];
+
+                nombreCombo.Items.Add(ip + " --> " + nombre);
+              
+            }
+        }
+        private int ejecutable(string _ip, string _pv, string _fecha, string _fecha_archivo)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            //ejem: getaudar -p 192.168.0.199 -i xml -o PV-0069-Playa-afip-del-01-09-2020-al-07-09-2020 -a 201001 201002
+            string sentencia = "getaudar -p " + _ip + " -i xml -o " + _pv + "-afip-del-" + _fecha + ".zip -a " + _fecha_archivo;
+
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine(sentencia);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+
+            while (!cmd.StandardOutput.EndOfStream)
+            {
+                string line = cmd.StandardOutput.ReadLine();
+                textBox1.Text += line + Environment.NewLine;
+
+            }
+            cmd.WaitForExit();
+
+            if (textBox1.Text.Contains("Fin de la descarga"))
+            {
+                return 1;
+            }
+            else 
+            {
+                return 0;
+            }
+          
+        }
+        private async void btn_iniciar_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            lbl_status.Visible = false;
+            this.BackColor = Color.FromKnownColor(KnownColor.Control);
+            if ((_ip != "") && (_punto_venta != "") && (_fecha != "") && (_fecha_archivo != ""))
+            {
+                tiempo = 0;
+                timer_tiempo.Enabled = true;
+                btn_iniciar.Enabled  = false;
+                progressBar1.Visible = true;
+
+                progressBar1.Maximum = 100;
+                progressBar1.Step = 1;
+                var someTask = Task<int>.Factory.StartNew(() => ejecutable(_ip, _punto_venta, _fecha, _fecha_archivo));
+                await someTask;
+                
+                if (someTask.Result == 1)
+                {
+                    progressBar1.Visible = false;
+                    btn_iniciar.Enabled = true;
+                    timer_tiempo.Enabled = false;
+                    Process.Start(Application.StartupPath);
+                    this.BackColor = Color.Green;
+                    lbl_status.Visible = true;
+                    lbl_status.Text = "Proceso Exitoso";
+                    
+                }
+                else
+                {
+                    progressBar1.Visible = false;
+                    btn_iniciar.Enabled = true;
+                    timer_tiempo.Enabled = false;
+                    this.BackColor = Color.Tomato;
+                    lbl_status.Visible = true;
+                    lbl_status.Text = "Proceso Fallido";
+
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Uno o mas campos estan sin Completar.");
+            }
+            
+        }
+       
+    }
+}
